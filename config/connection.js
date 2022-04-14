be14f942807eec8f1ad50b85b981fdadd51717e1
Niksharpkings <67552318+Niksharpkings@users.noreply.github.com>
2022-04-14 01:07:29 -0700

    :art: 13.2.5 password hashed in hooks whats added

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 // import the production environment variables
 require('dotenv').config();
 
-//Create a connect to our database, pass in your MYSQL username, password, host, poert, and database name
+//Create a connect to our database, pass in your MYSQL username, password, host, port, and database name
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
